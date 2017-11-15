25275f26b263120142343f0da2ae8d53baa02faa
Dohyun Yun <finsternis@me.com>
2017-11-16 01:45:27 +0900

    added sanic-jinja2 server with template page which includes bootstrap and d3 example

---
+++ server/app.py
@@ -0,0 +1,40 @@
+import sys
+
+from sanic import Sanic
+from sanic.response import json, html
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader('app', 'templates'),
+    autoescape=select_autoescape(['html', 'xml', 'tpl']),
+    enable_async=True
+)
+
+
+async def get_template(tpl, **kwargs):
+    template = env.get_template(tpl)
+    content = await template.render_async(kwargs)
+    return html(content)
+
+app = Sanic(__name__)
+app.static('/static', './static')
+
+
+@app.route("/")
+async def root(request):
+    return json({"bns root": "test"})
+
+
+@app.route("/bns")
+async def bns(request):
+    data = {}
+    content = await get_template('bns.html', title='Business Network Service', data=data)
+    return content
+
+
+if __name__ == "__main__":
+    port = 8000
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+    app.run(port=port)
